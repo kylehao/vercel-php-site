2d0bb83f57e929faede865f3abbeb006c44d4ac7
kylehao <kylehao@gmail.com>
2025-01-04 10:14:28 +0800

    up

--- api/photo-katong.php
+++ api/photo-katong.php
@@ -1,6 +1,6 @@
 <?php
 $domainName = $_SERVER['HTTP_HOST'];
-$img=file('https://'.$domainName.'/url-pic-katong.txt');
+$img=file('https://'.$domainName.'/url/url-pic-katong.txt');
 $url=array_rand($img);
 header("Location:".$img[$url]);
 ?>
--- api/photo-shima.php
+++ api/photo-shima.php
@@ -1,6 +1,6 @@
 <?php
 $domainName = $_SERVER['HTTP_HOST'];
-$img=file('https://'.$domainName.'/url-pic-shima.txt');
+$img=file('https://'.$domainName.'/url/url-pic-shima.txt');
 $url=array_rand($img);
 header("Location:".$img[$url]);
 ?>
