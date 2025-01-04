3fe890ba644d85c6f4fdb0968331e450880aa563
kylehao <kylehao@gmail.com>
2025-01-04 09:17:46 +0800

    Create test.php

---
+++ api/test.php
@@ -0,0 +1,19 @@
+<?php
+
+function read(...$filelist) {
+    $list = [];
+    foreach ($filelist as $file) {
+        $handle = fopen($file, 'r');
+        while (($line = fgets($handle)) !== false) {
+            array_push($list, trim($line));
+        }
+        fclose($handle);
+    }
+    return $list;
+}
+
+$domainName = $_SERVER['HTTP_HOST'];
+$list = read('https://'.$domainName.'/url/url-mp3-bg.txt');
+$url = $list[array_rand($list)];
+
+header("Location: {$url}");
